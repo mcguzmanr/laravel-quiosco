cd5c590e5e9679f9e08c8c7b094ba7ce36f4dbf1
Marcelo Guzman <mcguzmanr@gmail.com>
2023-05-08 13:54:36 -0400

    Deploment and first commit

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PedidoController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Almacenar ordenes
+    Route::apiResource('/pedidos', PedidoController::class);
+
+    Route::apiResource('/categorias', CategoriaController::class);
+    Route::apiResource('/productos', ProductoController::class);
+
+    Route::get('/productos-without-stock', [ProductoController::class, 'noStock']);
+    Route::put('/productos/update-stock/{producto}', [ProductoController::class, 'updateStock']);
+});
+
+
+
+
+// Autentificación
+Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
